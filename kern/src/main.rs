6ecc0b80c20db4eb46d801fc5882387969a3eb20
Valentin G. Badea <valentin.gabriel.badea@gmail.com>
2020-12-29 20:58:03 +0200

    kernel::main:: Add atags test

--- kern/src/main.rs
+++ kern/src/main.rs
@@ -18,22 +18,26 @@ pub mod console;
 pub mod fs;
 pub mod mutex;
 pub mod shell;
-
+use pi::timer::spin_sleep;
 use console::kprintln;
-
 use allocator::Allocator;
 use fs::FileSystem;
-
+use core::time::Duration;
+use pi::atags;
 #[cfg_attr(not(test), global_allocator)]
 pub static ALLOCATOR: Allocator = Allocator::uninitialized();
 pub static FILESYSTEM: FileSystem = FileSystem::uninitialized();
 
 fn kmain() -> ! {
-    unsafe {
-        ALLOCATOR.initialize();
-        FILESYSTEM.initialize();
-    }
 
-    kprintln!("Welcome to cs3210!");
+    // unsafe {
+    //     ALLOCATOR.initialize();
+    //     FILESYSTEM.initialize();
+    // }
+    spin_sleep(Duration::new(1,0));
+    let atags = atags::Atags::get();
+    for atag in atags{
+        kprintln!("{:#?}",atag);
+    }
     shell::shell("> ");
 }
