25c0d1d75c5781954d33b1fc5d910eec4eb9d9c7
Valentin G. Badea <valentin.gabriel.badea@gmail.com>
2020-12-29 20:23:04 +0200

    kern::panic:: Add panic for kernel

--- kern/src/init/panic.rs
+++ kern/src/init/panic.rs
@@ -1,6 +1,33 @@
+use crate::console::kprintln;
 use core::panic::PanicInfo;
 
+const MESSAGE:&str ="           (
+    (      )     )
+      )   (    (
+     (          `
+ .-\"\"^\"\"\"^\"\"^\"\"\"^\"\"-.
+(//\\//\\//\\//\\//\\//\\//)
+~\\^^^^^^^^^^^^^^^^^^/~
+  `================`
+
+ The pi is overdone.
+
+---------- PANIC ---------- ";
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    kprintln!("{}", MESSAGE);
+    if let Some(location) = info.location() {
+            kprintln!("panic occurred in file '{}' at line {} col {}",
+                location.file(),
+                location.line(),
+                location.column()
+            );
+        } else {
+            kprintln!("panic occurred but can't get location information...");
+        }
+    if let Some(s) = info.payload().downcast_ref::<&str>() {
+            kprintln!("panic occurred: {:?}", s);
+    }
     loop {}
 }
